288e4e03ba31fd2700ff15be11bf6124a1296f6b
wildan3105 <winistein@gmail.com>
2024-02-10 10:14:56 +0700

    prettier

--- src/libs/sleep/index.ts
+++ src/libs/sleep/index.ts
@@ -1,4 +1,4 @@
 export const sleep = async (ms: number): Promise<void> => {
-    console.log(`new build at ${new Date()}`) ;
     return new Promise((resolve) => setTimeout(resolve, ms));
+    console.log(` aaa a`);
 };
